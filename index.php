0428da70f4a203d2bcad61638860dc0598f22693
Anlimah <y.m.ratty7@gmail.com>
2023-06-15 21:17:19 +0000

    Ugrading api

--- index.php
+++ index.php
@@ -4,35 +4,71 @@ require_once('bootstrap.php');
 
 use Src\Controller\USSDHandler;
 use Src\Controller\PaymentController;
+use Src\Controller\ExposeDataController;
 
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
-$api_endpoint = '/' . basename($request_uri);
-
-header("Content-Type: application/json");
-echo json_encode(array("uri" => $request_uri, "endpoint" => $api_endpoint));
-exit();
+//echo json_encode(array("uri" => $request_uri, "endpoint" => $endpoint));
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case 'POST':
         $_POST = json_decode(file_get_contents("php://input"), true);
         $response = array();
-        $payData = array();
 
-        if (!empty($_POST)) $response = (new USSDHandler($_POST))->run();
+        if (!empty($_POST)) {
 
-        if (isset($response["data"])) {
-            $payData = $response["data"];
-            //unset($response["data"]);
+            $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+            $endpoint = '/' . basename($request_uri);
+
+
+            switch ($endpoint) {
+
+                case 'forms':
+                    break;
+
+                case 'buy':
+                    break;
+
+                case 'status':
+                    break;
+
+                case 'ussd':
+
+                    $payData = array();
+
+                    if (!empty($_POST)) $response = (new USSDHandler($_POST))->run();
+
+                    if (isset($response["data"])) $payData = $response["data"];
+
+                    if (!empty($payData)) {
+                        sleep(8);
+                        (new PaymentController())->orchardPaymentControllerB($payData);
+                    }
+
+                    break;
+
+                case 'confirm':
+
+                    $expose = new ExposeDataController();
+                    $expose->requestLogger(json_encode($_POST));
+
+                    if (!empty($_POST)) {
+                        $transaction_id = $expose->validatePhone($_POST["trans_ref"]);
+                        $data = $expose->confirmPurchase($transaction_id);
+                        $expose->requestLogger($transaction_id . " - " . $data[0]["message"]);
+                    }
+
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
         }
 
-        header("Content-Type: application/json");
-        echo json_encode($response);
+        break;
 
-        if (!empty($payData)) {
-            sleep(8);
-            (new PaymentController())->orchardPaymentControllerB($payData);
-        }
-
+    case 'GET':
+        header("Content-Type: text/html");
+        require_once 'advert.html';
         break;
 
     default:
